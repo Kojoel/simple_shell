e718294950c52a41ab12f7d464b71b2eb7d1fd6a
Kojoel <nanaabruquah1s@gmail.com>
2023-05-21 01:01:52 +0000

    Starting of with beautiful code in Simple shell

---
+++ beautifulcode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * printf - function to display beautiful code
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("This code is such a beauty :)");
+
+return (0);
+}
